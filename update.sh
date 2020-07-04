397469829930d7255aa5b951f01455d79b340f9e
2336604775 <2336604775@qq.com>
2020-07-04 21:09:08 +0800

    更新(又要鸽了)

--- update.sh
+++ update.sh
@@ -11,11 +11,10 @@ if [ "$ver" == "$var" ]; then
   echo -e "当前为最新版本"
 else
   echo -e "正在更新"
-  cd ~
-  rm $PREFIX/bin/dp 1>/dev/null 2>&1
+  cd $PREFIX/bin
+  rm dp 1>/dev/null 2>&1
   wget http://hackerror.top/bowh/dp
-  mv dp $PREFIX/bin
-  chmod +x $PREFIX/bin/dp
+  chmod +x dp
   echo "更新完成"
 fi
 echo -e "欢迎加入QQ群讨论:902795006"
